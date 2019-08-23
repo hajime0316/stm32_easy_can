b84dc89095ee8b426eb82be7beebea894f8b7c6c
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-08-23 20:32:12 +0900

    First commit

---
+++ stm32_easy_can.h
@@ -0,0 +1,27 @@
+/* 
+ * File:    stm32_easy_can.h
+ * version: ver02.00 (2019/04/01)
+ * Author:  hajime
+ *
+ * Created on 2019/03/26, 21:42
+ */
+
+#ifndef STM32_EASY_CAN_H_
+#define STM32_EASY_CAN_H_
+
+#include "main.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void stm32_easy_can_init(CAN_HandleTypeDef *hcan, int acceptance_filter, int filter_mask);
+void stm32_easy_can_get_receive_message(int *receive_id, int *receive_dlc, unsigned char receive_message[]);
+int stm32_easy_can_transmit_message(int transmit_id, int transmit_dlc, const unsigned char transmit_message[]);
+void stm32_easy_can_interrupt_handler(void); // 受信割り込みハンドラ(受信割り込みが発生すると呼ばれる．自分で定義する必要がある)
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* STM32_EASY_CAN_H_ */
